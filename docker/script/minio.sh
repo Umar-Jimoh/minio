97e131359661cf3b4d91eb9592620dd277440878
Umar Jimoh <umarjimoh@hotmail.com>
2025-04-19 16:38:46 +0100

    Add 0.0.0.0

--- docker/script/minio.sh
+++ docker/script/minio.sh
@@ -6,5 +6,5 @@ echo "MINIO_ROOT_PASSWORD: $MINIO_ROOT_PASSWORD"
 
 # Start MinIO with correct interface binding
 exec minio server /data \
-  --console-address ":9001" \
-  --address ":9000"
+  --console-address "0.0.0.0:9001" \
+  --address "0.0.0.0:9000"
